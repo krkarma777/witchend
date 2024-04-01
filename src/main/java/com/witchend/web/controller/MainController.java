2c82cbb90ddb05214a4802b97c43626c4ee8cdcd
krkarma777 <krkarma777@gmail.com>
2024-04-02 01:32:27 +0900

    feat: Added main page controller and view
    - Created MainController class to handle requests for "/" endpoint
    - Added main.html view file to display a simple test page

---
+++ src/main/java/com/witchend/web/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.witchend.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "main";
+    }
+}
