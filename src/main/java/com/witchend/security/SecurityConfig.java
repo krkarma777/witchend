4c3c1454fe3f5d7406d743427f4558e6c78c91e0
krkarma777 <krkarma777@gmail.com>
2024-04-03 13:38:09 +0900

    Refine SecurityConfig and update server port in application properties
    - Updated SecurityConfig to remove redundant authenticated request matcher, enhancing configuration clarity.
    - Changed server.port from 8091 to 8090 in application properties to align with deployment strategy.
    - Minor adjustments to application properties for consistency and readability.

--- src/main/java/com/witchend/security/SecurityConfig.java
+++ src/main/java/com/witchend/security/SecurityConfig.java
@@ -1,24 +1,24 @@
 package com.witchend.security;
 
-
 import com.witchend.domain.sevice.security.CustomOAuth2UserService;
 import com.witchend.security.handler.CustomAuthenticationSuccessHandler;
 import com.witchend.security.jwt.JWTFilter;
 import com.witchend.security.jwt.JWTUtil;
 import com.witchend.security.jwt.LoginFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -51,7 +51,7 @@ public class SecurityConfig {
                                 .requestMatchers("/", "/api/v1/auth/**", "/oauth2/**", "/login/social").permitAll()
                                 .requestMatchers("/css/**", "/js/**", "/img/**").permitAll() // 정적 리소스 허용
                                 .requestMatchers("/register/**", "/registerProc", "/login/**", "/loginProc", "/").permitAll()
-                                .requestMatchers("").authenticated()
+//                                .requestMatchers("").authenticated()
                                 .requestMatchers("/admin/**").hasRole("ADMIN")
                                 .anyRequest().permitAll()
                 );
