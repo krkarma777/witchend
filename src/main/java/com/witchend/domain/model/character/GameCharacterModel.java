c51fdae1c213a99a9847c413f133fc13d26e19c6
krkarma777 <krkarma777@gmail.com>
2024-04-06 20:49:33 +0900

    feat: Define GameCharacterModel interface for character attributes
    Created GameCharacterModel interface to standardize the structure of character models in the game. This interface defines essential attributes that all character classes must implement, ensuring consistency across different types.

---
+++ src/main/java/com/witchend/domain/model/character/GameCharacterModel.java
@@ -0,0 +1,16 @@
+package com.witchend.domain.model.character;
+
+public interface GameCharacterModel {
+
+    Integer getHealth();
+
+    Integer getStrength();
+
+    Integer getAgility();
+
+    Integer getDexterity();
+
+    Integer getHunger();
+
+    Integer getDefense();
+}
