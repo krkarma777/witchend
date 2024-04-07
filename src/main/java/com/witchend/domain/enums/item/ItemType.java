f54bcbe869f61483071270442a4fa0379470a224
krkarma777 <krkarma777@gmail.com>
2024-04-07 15:30:54 +0900

    기능: 아이템 유형 및 희귀도 수준을 위한 열거형 추가
    - 게임 내에서 다양한 유형의 아이템을 식별하고 처리하기 위해 WEAPON, ARMOR, POTION, KEY_ITEM으로 분류하는 ItemType 열거형을 도입하였습니다.
    - 아이템의 희귀도 수준: COMMON, UNCOMMON, RARE, EPIC, LEGENDARY를 나타내는 Rarity 열거형을 추가하여, 아이템 희귀도와 게임플레이에 미치는 영향에 대한 구조적인 접근을 가능하게 합니다.

---
+++ src/main/java/com/witchend/domain/enums/item/ItemType.java
@@ -0,0 +1,8 @@
+package com.witchend.domain.enums.item;
+
+public enum ItemType {
+    WEAPON,
+    ARMOR,
+    POTION,
+    KEY_ITEM // 게임 내에서 필요한 아이템 유형을 추가
+}
---
+++ src/main/java/com/witchend/domain/enums/item/Rarity.java
@@ -0,0 +1,9 @@
+package com.witchend.domain.enums.item;
+
+public enum Rarity {
+    COMMON,
+    UNCOMMON,
+    RARE,
+    EPIC,
+    LEGENDARY
+}
