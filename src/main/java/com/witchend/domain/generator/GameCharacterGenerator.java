c07374bdc07b9b6450ef13e31f57c2d56aebdbb6
krkarma777 <krkarma777@gmail.com>
2024-04-07 16:47:16 +0900

    Integrate Inventory creation with GameCharacter generation
    Enhanced the GameCharacterGenerator component by integrating inventory creation alongside game character generation. Now, when a new game character is created, an associated inventory is also instantiated and saved. This addition streamlines the process of setting up a new character for the game, ensuring that each character has their inventory setup from the start.

--- src/main/java/com/witchend/domain/generator/GameCharacterGenerator.java
+++ src/main/java/com/witchend/domain/generator/GameCharacterGenerator.java
@@ -1,11 +1,13 @@
 package com.witchend.domain.generator;
 
 import com.witchend.domain.entity.user.GameCharacter;
+import com.witchend.domain.entity.user.Inventory;
 import com.witchend.domain.entity.user.User;
 import com.witchend.domain.enums.CharacterClass;
 import com.witchend.domain.model.character.Diamond;
 import com.witchend.domain.model.character.Jade;
 import com.witchend.domain.sevice.GameCharacterService;
+import com.witchend.domain.sevice.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -16,10 +18,14 @@ import org.springframework.web.server.ResponseStatusException;
 public class GameCharacterGenerator {
 
     private final GameCharacterService gameCharacterService;
+    private final InventoryService inventoryService;
 
     public void generate(CharacterClass characterClass, User user) {
         GameCharacter gameCharacter = getGameCharacter(characterClass, user);
-        gameCharacterService.save(gameCharacter);
+        GameCharacter savedGameCharacter = gameCharacterService.save(gameCharacter);
+
+        Inventory inventory = new Inventory(savedGameCharacter);
+        inventoryService.save(inventory);
     }
 
     private GameCharacter getGameCharacter(CharacterClass characterClass, User user) {
